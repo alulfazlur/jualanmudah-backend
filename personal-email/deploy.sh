61559d6367a2706e6e4265fdf478de278be28ccb
adul11s <romli@alterra.id>
2020-07-02 20:56:31 +0700

    nambahin sudo di dockerfile

--- personal-email/deploy.sh
+++ personal-email/deploy.sh
@@ -5,7 +5,7 @@ ssh-add -k ~/.ssh/id_rsa &&
 
 source ~/.profile
 echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
-docker stop slytherin
-docker rm slytherin
-docker rmi adul11/slytherin:latest
-docker run -d --name slytherin -p 5050:5050 adul11/slytherin:latest
+sudo docker stop slytherin
+sudo docker rm slytherin
+sudo docker rmi adul11/slytherin:latest
+sudo docker run -d --name slytherin -p 5050:5050 adul11/slytherin:latest
